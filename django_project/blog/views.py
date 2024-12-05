a5812e62709e6f17c2987b3e9f5590086fa1dceb
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2024-12-05 13:05:20 +0100

    Axe graphique

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,39 +1,45 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.db.models import Count, Sum
 from django.db.models.functions import TruncDate
-from django.db.models import Count
-from .models import Canva, Pixel, UserAction, PixelModification
+from django.utils.timezone import now
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-import json
-from django.utils.timezone import now
 from datetime import timedelta
+import json
+from .models import Canva, Pixel, UserAction, PixelModification
+import matplotlib.pyplot as plt
+import io
+import base64
+from matplotlib.dates import DateFormatter
+from matplotlib.ticker import MaxNLocator
 from django.contrib.auth.models import User
-from django.db.models import Sum
-from django.shortcuts import get_object_or_404, render
+
+# View for the home page
 def home(request):
     canvases = Canva.objects.annotate(
         total_modifications=Sum('useraction__modification_count')
     ).order_by('-total_modifications')
 
-    # Préparer la grille pour l'affichage
+    # Prepare the grid for each canvas
     for canva in canvases:
         pixels = canva.pixels.all()
         grid = [[None for _ in range(canva.sizeWidth)] for _ in range(canva.sizeHeight)]
         for pixel in pixels:
             grid[pixel.y][pixel.x] = pixel
-        canva.grid = grid  # Ajout de la grille au contexte de chaque canva
+        canva.grid = grid  # Add grid to the context of each canvas
 
     return render(request, 'blog/home.html', {'canvases': canvases})
 
-
+# List view for canvases
 class CanvaListView(ListView):
     model = Canva
     template_name = 'blog/home.html'
     context_object_name = 'canvases'
 
-
+# Detail view for a single canvas
 class CanvaDetailView(DetailView):
     model = Canva
 
@@ -45,6 +51,7 @@ class CanvaDetailView(DetailView):
         for pixel in pixels:
             grid[pixel.y][pixel.x] = pixel
 
+        # Get the last user action for the current user
         user_action = UserAction.objects.filter(user=self.request.user, canva=canva).first()
         if user_action:
             time_since_last_action = now() - user_action.last_modified
@@ -55,7 +62,7 @@ class CanvaDetailView(DetailView):
         context['pixels'] = grid
         return context
 
-
+# Function to update pixel data
 @login_required
 def update_pixel(request, pk):
     canva = get_object_or_404(Canva, pk=pk)
@@ -103,8 +110,7 @@ def update_pixel(request, pk):
             'pixels': grid
         })
 
-
-
+# Create view for a new canvas
 class CanvaCreateView(LoginRequiredMixin, CreateView):
     model = Canva
     fields = ['title', 'sizeHeight', 'sizeWidth', 'timer']
@@ -115,14 +121,14 @@ class CanvaCreateView(LoginRequiredMixin, CreateView):
         size_width = int(self.request.POST.get('sizeWidth', 5))
         size_height = int(self.request.POST.get('sizeHeight', 5))
 
-        context['pixels'] = [[{'x': x, 'y': y, 'color': '#FFFFFF'} for x in range(size_width)] for y in
-                             range(size_height)]
+        context['pixels'] = [[{'x': x, 'y': y, 'color': '#FFFFFF'} for x in range(size_width)] for y in range(size_height)]
         return context
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         response = super().form_valid(form)
 
+        # Save the pixel data
         pixel_data = json.loads(self.request.POST.get('pixel_data', '[]'))
         for pixel in pixel_data:
             Pixel.objects.update_or_create(
@@ -134,7 +140,7 @@ class CanvaCreateView(LoginRequiredMixin, CreateView):
 
         return response
 
-
+# Update view for an existing canvas
 class CanvaUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Canva
     fields = ['title', 'sizeHeight', 'sizeWidth', 'timer']
@@ -146,7 +152,7 @@ class CanvaUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         return self.request.user == self.get_object().author
 
-
+# Delete view for a canvas
 class CanvaDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Canva
     success_url = '/'
@@ -154,48 +160,34 @@ class CanvaDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         return self.request.user == self.get_object().author
 
-from django.db.models import Count
-from django.db.models.functions import TruncDate
-from django.utils.timezone import now
-from datetime import timedelta
-
-import matplotlib.pyplot as plt
-import io
-import base64
-
-import matplotlib.pyplot as plt
-import io
-import base64
-from matplotlib.dates import DateFormatter
-
-import matplotlib.pyplot as plt
-import io
-import base64
-from matplotlib.dates import DateFormatter
-from matplotlib.ticker import MaxNLocator
-
+# Statistics view for pixel modifications
 def statistic(request):
     canva_id = request.GET.get('canva_id')
     canva = get_object_or_404(Canva, id=canva_id) if canva_id else None
 
     if canva:
+        # User rankings based on modifications
         user_rankings = UserAction.objects.filter(canva=canva) \
             .values('user__username', 'user__id') \
             .annotate(modification_count=Sum('modification_count')) \
             .order_by('-modification_count')
 
+        # Total modifications
         total_modifications = UserAction.objects.filter(canva=canva).aggregate(total=Sum('modification_count'))['total'] or 0
 
+        # Date range for the canvas
         start_date = canva.date_posted.date()
         end_date = now().date()
         date_range = [start_date + timedelta(days=i) for i in range((end_date - start_date).days + 1)]
 
+        # Pixel modifications by date
         pixel_modifications_by_date = PixelModification.objects.filter(pixel__canva=canva) \
             .annotate(date=TruncDate('modified_at')) \
             .values('date') \
             .annotate(total_modifications=Count('id')) \
             .order_by('date')
 
+        # Prepare the pixel modification data
         modification_dict = {mod['date']: mod['total_modifications'] for mod in pixel_modifications_by_date}
 
         pixel_modifications_with_all_dates = []
@@ -205,11 +197,11 @@ def statistic(request):
                 'total_modifications': modification_dict.get(date, 0)
             })
 
-        # Préparer les données pour le graphique
+        # Prepare data for the chart
         dates = [item['date'] for item in pixel_modifications_with_all_dates]
         modifications = [item['total_modifications'] for item in pixel_modifications_with_all_dates]
 
-        # Supprimer les doublons dans les dates (en gardant seulement la première occurrence)
+        # Remove duplicates in dates
         unique_dates = []
         unique_modifications = []
         for i in range(len(dates)):
@@ -217,22 +209,19 @@ def statistic(request):
                 unique_dates.append(dates[i])
                 unique_modifications.append(modifications[i])
 
-        # Générer le graphique
+        # Generate the plot
         plt.figure(figsize=(10, 5))
         plt.plot(unique_dates, unique_modifications, marker='o', color='b')
-        plt.title(f"Pixel Modifications for {canva.title}", fontsize=16)
-        plt.xlabel("Date", fontsize=12)
-        plt.ylabel("Total Modifications", fontsize=12)
-
-        # Formater les dates sur l'axe X
-        plt.gca().xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))  # Format de la date (année-mois-jour)
-        plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))  # Gérer les ticks de manière appropriée
-        plt.xticks(rotation=45)  # Faire pivoter les dates pour éviter qu'elles ne se chevauchent
-
+        plt.title(f"The evolution of the Canva {canva.title}", fontsize=16)
+        plt.xlabel("Date since graphic was created", fontsize=12)
+        plt.ylabel("Total Modifications by day", fontsize=12)
+        plt.gca().xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+        plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+        plt.xticks(rotation=45)
         plt.grid(True)
         plt.tight_layout()
 
-        # Sauvegarder l'image dans un buffer
+        # Save the plot in a buffer
         buffer = io.BytesIO()
         plt.savefig(buffer, format='png')
         buffer.seek(0)
@@ -244,12 +233,12 @@ def statistic(request):
             'user_rankings': user_rankings,
             'total_modifications': total_modifications,
             'pixel_modifications_by_date': pixel_modifications_with_all_dates,
-            'chart': image_base64,  # Ajouter le graphique encodé
+            'chart': image_base64,
         })
 
     return render(request, 'blog/statistic.html', {'canva': canva})
 
-
+# User profile view
 def user_profile(request, user_id):
     user = get_object_or_404(User, id=user_id)
     user_actions = UserAction.objects.filter(user=user).values(
